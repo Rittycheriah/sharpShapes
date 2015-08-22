e80d7a854bedd631d7b81b81161fe8ba93eccd88
Rainu Ittycheriah <rittycheriah@gmail.com>
2015-08-21 17:39:44 -0700

    Squares Test and Class passing and working

---
+++ Shapes/Squares.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Squares : Quadrilateral
+    {
+        public override float Area()
+        {
+            return Side1 * Side1;
+        }
+
+        public override float Perimeter()
+        {
+            return Side1 * 4;
+        }
+    }
+}
---
+++ UnitTestShapes/SquareTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestShapes
+{
+    [TestClass]
+    public class SquareTest
+    {
+        [TestMethod]
+        public void SquareAreaTest()
+        {
+            var testSq = new Squares();
+            testSq.Side1 = 10;
+
+            Assert.AreEqual(testSq.Area(), 100);
+        }
+
+        [TestMethod]
+        public void SquarePerimeterTest()
+        {
+            var testSq = new Squares();
+            testSq.Side1 = 10;
+
+            Assert.AreEqual(testSq.Perimeter(), 40);
+        }
+    }
+}

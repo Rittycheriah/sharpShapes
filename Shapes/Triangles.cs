d68d36fd7fffca589e89bc0904c65da52b4baa7f
Rainu Ittycheriah <rittycheriah@gmail.com>
2015-08-22 09:16:38 -0700

    Wrote a Triangle class with basic Area, Perimeter funcs

---
+++ Shapes/Triangles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Triangles : Shape
+    {
+        public float side1 { get; set; }
+        public float side2 { get; set; }
+        public float side3 { get; set; }
+
+        public override float Area()
+        {
+            float forPerimeterCalc = this.Perimeter() / 2;
+            float forPA = forPerimeterCalc - side1;
+            float forPB = forPerimeterCalc - side2;
+            float forPC = forPerimeterCalc - side3;
+            float takeSqRt = forPerimeterCalc * forPA * forPB * forPC;
+
+            return (float)Math.Sqrt(takeSqRt);
+        }
+
+        public override float Perimeter()
+        {
+            return side1 + side2 + side3;
+        }
+    }
+}
---
+++ UnitTestShapes/TriangleTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestShapes
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void TestsTrianglePerimeter()
+        {
+            Triangles myTriangle = new Triangles();
+            myTriangle.side1 = 2;
+            myTriangle.side2 = 3;
+            myTriangle.side3 = 4;
+
+            Assert.AreEqual(myTriangle.Perimeter(), 9);
+        }
+
+        [TestMethod]
+        public void TestsTriangleArea()
+        {
+            Triangles myTriangle = new Triangles();
+            myTriangle.side1 = 3;
+            myTriangle.side2 = 4;
+            myTriangle.side3 = 5;
+
+            Assert.AreEqual(myTriangle.Area(), 6);
+        }
+    }
+}

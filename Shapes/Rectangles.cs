8f1ecbfa065d5d95e2c4a904d2e14351e956ae67
Rainu Ittycheriah <rittycheriah@gmail.com>
2015-08-21 17:57:06 -0700

    rectangles tests and class working

---
+++ Shapes/Rectangles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangles : Quadrilateral
+    {
+        public override float Area()
+        {
+            return Side1 * Side2;
+        }
+
+        public override float Perimeter()
+        {
+            return (Side1 * 2) + (Side2 * 2);
+        }
+    }
+}
---
+++ UnitTestShapes/RectangleTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestShapes
+{
+    [TestClass]
+    public class RectangleTest
+    {
+        [TestMethod]
+        public void RectangleAreaTest()
+        {
+            var testRectangle = new Rectangles();
+            testRectangle.Side1 = 8;
+            testRectangle.Side2 = 4;
+
+            Assert.AreEqual(testRectangle.Area(), 32);
+        }
+
+        [TestMethod]
+        public void RectanglePerimeterTest()
+        {
+            var testRectangle = new Rectangles();
+            testRectangle.Side1 = 8;
+            testRectangle.Side2 = 4;
+
+            Assert.AreEqual(testRectangle.Perimeter(), 24);
+        }
+    }
+}
